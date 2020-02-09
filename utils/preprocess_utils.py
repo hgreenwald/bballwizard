e34564d76ecb0ce5500600a147e41cb0a06b8d78
hgreenwald <jhgreenwald@gmail.com>
2020-02-09 15:09:43 +0100

    Created using Colaboratory

---
+++ utils/preprocess_utils.py
@@ -0,0 +1,45 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "name": "preprocess_utils.py",
+      "provenance": [],
+      "collapsed_sections": [],
+      "toc_visible": true,
+      "mount_file_id": "1e2NRAtBywS0-oWfNvAh7KHDIbN8ijPnc",
+      "authorship_tag": "ABX9TyNWJsc0fxIk1wo6eZavwnWH",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/hgreenwald/bballwizard/blob/master/utils/preprocess_utils.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "metadata": {
+        "id": "si2iq7nlEhmG",
+        "colab_type": "code",
+        "colab": {}
+      },
+      "source": [
+        "def hello_world():\n",
+        "  print(\"Hello world\")"
+      ],
+      "execution_count": 0,
+      "outputs": []
+    }
+  ]
+}
